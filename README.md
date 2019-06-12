333d591a232c9d0ad7bf9d2b6c6bd5047214169c
Zina Ramirez <zina@zinaramirez.com>
2019-06-12 14:10:46 -0400

    Initialization of MERN stack and user model

---
+++ README.md
@@ -0,0 +1,19 @@
+# Financer
+A financing application built with the MERN stack.
+
+## Quick Start
+### Prerequisites
+* NodeJS
+* MongoDB database
+### Installation
+``` bash
+npm install
+```
+After installing, edit ```server/config.js.example``` to contain the MongoDB link information.
+Rename the file to **config.js**.
+### Running
+Starting the application uses ```concurrently``` to run both the Node server and React frontend.
+``` bash
+npm start
+```
+
---
+++ server/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require(bcrypt);
+const SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
+    username : { type: String, required: true, index: { unique: true}},
+    email    : { type: String, index: { unique: true}},
+    password : { type: String, required: true}
+
+}, {timestamps: true});
+
+/** 
+* Middleware hashes passwords before saving to db
+* Note: only works on save(), not update()
+*/
+UserSchema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+/**
+ * Validator for passwords
+ * 
+ * @param  {String} Password to compare
+ * @param  {Function} Callback function
+ * @return Calls the callback function
+ */
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', UserSchema);
