b3a4080e9b64b9959db98b155acec68f10d4e99f
Zina Ramirez <zina@zinaramirez.com>
2019-06-12 16:48:55 -0400

    Preparing for front end scaffolding

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+    id: 0,
+    message: null,
+  };
+
+  componentDidMount() {
+    this.getDataFromDb();
+  }
+
+  getDataFromDb = () => {
+    axios.get('http://localhost:3001/user/login')
+      .then(res => {
+        const data = res.data;
+        this.setState({data: data});
+    });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    
+    
+    return (
+      <div>Hallo Wurld<br/>{this.state.data}</div>
+    );
+  }
 }
 
 export default App;
